e082d605035ed5c8a814eb385b43fc3cf4b99e9f
Jordano <lechuga.letucce389203@gmail.com>
2025-01-25 11:21:00 -0300

    Funcion y lista creados

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ let amigosInsertados = [];
 // funcion para agregar amigos a la lista
 
 function agregarAmigo(nombre){
-    valor = document.getElementById('amigo').value;// obtengo el valor del input
-    amigosInsertados.push(valor);// agrego el valor al array
-    console.log(amigosInsertados);
+    let inputAmigo = document.getElementById('amigo');
+    if (inputAmigo.value == "") {
+        alert("Debe ingresar un nombre");
+        return;
+    } else {
+        valor = inputAmigo.value;// obtengo el valor del input
+        amigosInsertados.push(valor);// agrego el valor al array
+        console.log(amigosInsertados);
+        document.getElementById('amigo').value = "";// limpio el input
+    }
+    
 }
